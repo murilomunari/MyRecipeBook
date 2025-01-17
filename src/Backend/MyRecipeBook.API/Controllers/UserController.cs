9593a15954dce3d84b85eb3224f8257f55fdff22
mu_bi <munarimurilo441@gmail.com>
2025-01-17 18:00:51 -0300

    Adiciona endpoint de registro de usuário com suporte a JSON no corpo da requisição

---
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+
+namespace MyRecipeBook.API.Controllers;
+
+[Route("[controller]")]
+[ApiController]
+public class UserController : ControllerBase
+{
+    [HttpPost]
+    public IActionResult Register(RequestRegisterUserJson request)
+    {
+        return Created();
+    }
+}
---
+++ src/Shared/MyRecipeBook.Communication/Requests/RequestRegisterUserJson.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRecipeBook.Communication.Requests;
+
+public class RequestRegisterUserJson
+{
+    public String name { get; set; } = string.Empty;
+    
+    public String email { get; set; } = string.Empty;
+    
+    public String password { get; set; } = string.Empty;
+    
+    
+}
