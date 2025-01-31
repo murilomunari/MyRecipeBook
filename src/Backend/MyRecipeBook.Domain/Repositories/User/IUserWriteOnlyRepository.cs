1fc8fc78fde6dbbccd52c52f3f65712ecee23897
mu_bi <munarimurilo441@gmail.com>
2025-01-31 11:45:16 -0300

    Salvando o usuario no banco e comecando a injeção de dependencia

---
+++ src/Backend/MyRecipeBook.Domain/Repositories/User/IUserWriteOnlyRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyRecipeBook.Domain.Repositories.User
+{
+    internal interface IUserWriteOnlyRepository
+    {
+    }
+}
